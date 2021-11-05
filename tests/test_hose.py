227713fb5bf190e92ceab2f9aff35d6649640a07
vuthanhdatt <80867452+vuthanhdatt@users.noreply.github.com>
2021-11-05 15:31:27 +0700

    Rename test folder

--- tests/test_hose.py
+++ tests/test_hose.py
@@ -11,5 +11,5 @@ cookies = ast.literal_eval(os.getenv('COOKIES'))
 
 if __name__ == '__main__':
     hose = Hose(cookies)
-    print(hose.market_index('2021-04-04','2021-07-07'))
+    print(hose.all_company_info())
 
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,16 +1,17 @@
+from datetime import datetime
 import os
 import ast
 import sys
 from dotenv import load_dotenv
 sys.path.append('vnstock_data')
-from hnx import Hnx
+from main import VnStock
 
 #Load cookies for testing
 load_dotenv()
 cookies = ast.literal_eval(os.getenv('COOKIES'))
-start = '2021-04-04'
-end = '2021-11-01'
 
 if __name__ == '__main__':
-    hnx = Hnx(cookies)
-    print(hnx.market_index(start, end))
+    start = datetime.now()
+    vn = VnStock(cookies)
+    print(vn.industry_type)
+    print(datetime.now()-start)
